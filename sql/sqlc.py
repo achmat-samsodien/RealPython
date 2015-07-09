58db8d3a5f2d33fe44f3f17266a024c475715558
Achmat <achmatsamsodien@gmail.com>
2015-07-09 11:45:05 +0200

    SQL Inserts

--- sql/sqlc.py
+++ sql/sqlc.py
@@ -7,7 +7,13 @@ with sqlite3.connect('new.db') as connection:
 
 #insert multiple records using a tuple
 
-cities = [('Boston', 'MA','9120000')
-          ('Chicago','IL','3245666')
-          ()]
+cities = [
+          ('Boston', 'MA',9120000),
+          ('Chicago','IL',3245666),
+          ('Houston','TX',3200000),
+          ('Denver','CL',2400000)
+         ]
 
+#insert into datatable
+
+c.executemany('INSERT INTO population VALUES(?,?,?)',cities)
